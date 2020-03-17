437155491c8678813431be14d79d6e4229998a03
Miloslav Žežulka <mzezulka@localhost.localdomain>
2020-03-17 14:18:39 +0100

    Update TracingInterceptor so that it uses TEXT_MAP builtin format span context.

--- src/main/java/org/jboss/ejb/client/TracingInterceptor.java
+++ src/main/java/org/jboss/ejb/client/TracingInterceptor.java
@@ -4,21 +4,24 @@ import org.jboss.ejb.client.annotation.ClientInterceptorPriority;
 
 import io.opentracing.Span;
 import io.opentracing.propagation.Format;
-import io.opentracing.propagation.TextMapInjectAdapter;
+import io.opentracing.propagation.TextMap;
 import io.opentracing.util.GlobalTracer;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ClientInterceptorPriority(TracingInterceptor.PRIORITY)
 public class TracingInterceptor implements EJBClientInterceptor {
 
-    public static final int PRIORITY = ClientInterceptorPriority.JBOSS_AFTER + 175; 
-    
+    public static final int PRIORITY = ClientInterceptorPriority.JBOSS_AFTER + 175;
+
     @Override
     public void handleInvocation(EJBClientInvocationContext context) throws Exception {
         Span span = GlobalTracer.get().activeSpan();
         // EJB client shouldn't be responsible for creating 
         // any spans (if it is not instrumenting the EJB client itself)
-        if(span != null) {
-            GlobalTracer.get().inject(span.context(), Format.Builtin.TEXT_MAP_INJECT, new TextMapInjectAdapter(context.getContextData()));   
+        if (span != null) {
+            GlobalTracer.get().inject(span.context(), Format.Builtin.TEXT_MAP, new TextMapAdapter(context.getContextData()));
         }
         context.sendRequest();
     }
@@ -26,5 +29,32 @@ public class TracingInterceptor implements EJBClientInterceptor {
     @Override
     public Object handleInvocationResult(EJBClientInvocationContext context) throws Exception {
         return context.getResult();
+    }
+
+    /**
+     * Instead of using {@link io.opentracing.propagation.TextMapAdapter}, we need to be
+     * able to adapt to the type Map&lt;String, Object&gt; since that is the carrier
+     * type for the EJB client.
+     */
+    public static class TextMapAdapter implements TextMap {
+
+        private final Map<String, String> map;
+
+        public TextMapAdapter(Map<String, ? super String> map) {
+            this.map = map.entrySet()
+                    .stream()
+                    .filter(x -> x.getValue() instanceof String)
+                    .collect(Collectors.toMap(Map.Entry::getKey, x -> (String) x.getValue()));
+        }
+
+        @Override
+        public void put(String key, String value) {
+            map.put(key, value);
+        }
+
+        @Override
+        public Iterator<Map.Entry<String, String>> iterator() {
+            return map.entrySet().iterator();
+        }
     }
 }
